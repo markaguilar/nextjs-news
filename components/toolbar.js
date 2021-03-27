d0a6be63a8ec8e471395bffa1dcf335dad3fd484
mark ferdinan aguilar <markferdinan.mfa@gmail.com>
2021-03-28 03:17:44 +0800

    added changes

--- components/toolbar.js
+++ components/toolbar.js
@@ -1,22 +1,16 @@
 import { useRouter } from 'next/router';
 import styles from '../styles/Toolbar.module.css';
+
 export const Toolbar = () => {
   const router = useRouter();
 
   return (
     <div className={styles.main}>
-      <div className='' onClick={() => router.push('/')}>
-        Home
-      </div>
-      <div className='' onClick={() => router.push('/feed/1')}>
-        Feed
-      </div>
-      <div className='' onClick={() => router.push('/eom')}>
-        EOM
-      </div>
+      <div onClick={() => router.push('/')}>Home</div>
+      <div onClick={() => router.push('/feed/1')}>Feed</div>
+      <div onClick={() => router.push('/eom')}>EOM</div>
       <div
-        className=''
-        onclick={() => (window.location.href = 'https://twitter.com/')}
+        onClick={() => (window.location.href = 'https://twitter.com/portexe')}
       >
         Twitter
       </div>
--- pages/eom.js
+++ pages/eom.js
@@ -1,20 +1,47 @@
+import Head from 'next/head';
 import styles from '../styles/EOM.module.css';
 import { Toolbar } from '../components/toolbar';
 
 export const EOM = ({ employee }) => {
   return (
-    <div className='page-container'>
-      <Toolbar />
-      <div className={styles.main}>
-        <h1>Employee of the month</h1>
+    <>
+      <Head>
+        <title>Employee Of The Month</title>
+        <meta
+          name='description'
+          content={`This month's employee of the month is ${employee.name}`}
+        />
 
-        <div className={styles.employeeOfTheMonth}>
-          <h3>{employee.name}</h3>
-          <h6>{employee.position}</h6>
-          <img src={employee.image} alt='employee image' />
+        <meta property='og:image' content={employee.image} />
+        <meta property='og:title' content='Employee Of The Month' />
+        <meta
+          property='og:description'
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+
+        <meta property='twitter:image' content={employee.image} />
+        <meta property='twitter:title' content='Employee Of The Month' />
+        <meta
+          property='twitter:description'
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+      </Head>
+
+      <div className='page-container'>
+        <Toolbar />
+
+        <div className={styles.main}>
+          <h1>Employee Of The Month</h1>
+
+          <div className={styles.employeeOfTheMonth}>
+            <h3>{employee.name}</h3>
+            <h6>{employee.position}</h6>
+            <img src={employee.image} alt='employee' />
+            <p>{employee.description}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
